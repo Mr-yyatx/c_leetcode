c4409e9f9a9d7d73489f5f92934d35bca98175ef
yyyzl <2582325957@qq.com>
2022-07-30 18:35:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# umi project
+
+## Getting Started
+
+Install dependencies,
+
+```bash
+$ yarn
+```
+
+Start the dev server,
+
+```bash
+$ yarn start
+```
+
+在src/.umi/core/route.ts引入使用config路由文件
---
+++ src/config/route.ts
@@ -0,0 +1,14 @@
+const routes = [
+  {
+    "path": "/",
+    "component": require('@/pages/index').default,
+    "exact": true
+  },
+  {
+    "path": "/home",
+    "component": require('@/pages/index').default,
+    "exact": true
+  }
+];
+
+export default routes
