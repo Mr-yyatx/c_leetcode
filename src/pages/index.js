7f2a4b11f8ef892cc2e0e6ed301eecac67d853d0
yyyzl <2582325957@qq.com>
2022-07-30 18:43:57 +0800

    gitignore

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import styles from './index.less';
-export default function IndexPage() {
-    return (_jsx("div", { children: _jsx("h1", { className: styles.title, children: "Page index" }) }));
-}
-//# sourceMappingURL=index.js.map
